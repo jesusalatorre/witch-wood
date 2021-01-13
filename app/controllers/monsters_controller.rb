9ea2be83d7cf5e83f9e9d9517bedb28510398910
Jesus Alatorre <chuyalatorre@gmail.com>
2021-01-13 16:08:54 -0600

    adds pagination, updates edit

--- app/controllers/monsters_controller.rb
+++ app/controllers/monsters_controller.rb
@@ -6,7 +6,7 @@ class MonstersController < ApplicationController
   end
 
   def index
-    @monsters = Monster.all
+    @monsters = Monster.paginate(page: params[:page], per_page: 4)
   end
 
   def new
