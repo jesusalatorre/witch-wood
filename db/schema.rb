079c92aefd9a375c94f6238fa8c82612446db265
Jesus Alatorre <chuyalatorre@gmail.com>
2021-01-21 12:14:22 -0600

    add about to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_14_213848) do
+ActiveRecord::Schema.define(version: 2021_01_21_180522) do
 
   create_table "monsters", force: :cascade do |t|
     t.string "name"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 2021_01_14_213848) do
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "about"
   end
 
 end
